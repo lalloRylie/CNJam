748abcbdbdf8bdf9c16445ccf9fe132e7dab2263
Rylie Lallo <Rylie Lallo>
2016-02-12 23:57:30 -0800

    Enemy prefab created, including scripts

---
+++ Assets/Scripts/Enemies/EnemyHealth.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyHealth : MonoBehaviour {
+
+    public int health = 1;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        //If health drops to zero, enter death state
+        if (health <= 0) {
+            GetComponent<EnemyBehavior>().enemyState = 4;
+        }
+	}
+}
