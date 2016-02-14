e1d48188d45ae226f68d4edb0e9785bb5956afb3
Devin Stewart <Devin Stewart>
2016-02-14 10:44:14 -0800

    added clouds

---
+++ Assets/Scripts/BackgroundScripts/Cloud_ScrollBy.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Cloud_ScrollBy : MonoBehaviour {
+
+    public float speed = -0.01f;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.Translate(new Vector3(speed, 0f, 0f) * Time.deltaTime);
+	}
+}
