c3164914aea2b7c00d991a1ed66699af3c0220a8
Rylie Lallo <Rylie Lallo>
2016-02-14 12:01:01 -0800

    Adding projectiles to animations

--- Assets/Scripts/PlayerScripts/Player_ControlAnimationState.cs
+++ Assets/Scripts/PlayerScripts/Player_ControlAnimationState.cs
@@ -8,6 +8,9 @@ public class Player_ControlAnimationState : MonoBehaviour
     GameObject playerGO;
     Player_Attack playerAttackScript;
 
+    public GameObject shockProjectile;
+    public GameObject shockBlastProjectile;
+
     // Use this for initialization
     void Start()
     {
@@ -38,5 +41,37 @@ public class Player_ControlAnimationState : MonoBehaviour
     public void ApplyHalfBoardWipeDamage()
     {
         playerAttackScript.HalfBoardWipe(playerAttackScript.halfBoardWipeSideOnLeft);
+    }
+
+    public void Shock()
+    {
+        GameObject projectile = GameObject.Instantiate<GameObject>(shockProjectile);
+
+        if (playerAttackScript.halfBoardWipeSideOnLeft)
+        {
+            //shoot blast left
+            
+        }
+        else
+        {
+            //shoot blast right
+            
+        }
+    }
+
+    public void ShockBlast()
+    {
+        GameObject projectile = GameObject.Instantiate<GameObject>(shockBlastProjectile);
+
+        if (playerAttackScript.halfBoardWipeSideOnLeft)
+        {
+            //shoot blast left
+            projectile.transform.position = new Vector3(transform.position.x - 2f, -1.1f, 0f);
+        }
+        else
+        {
+            //shoot blast right
+            projectile.transform.position = new Vector3(transform.position.x + 2f, -1.1f, 0f);
+        }
     }
 }
