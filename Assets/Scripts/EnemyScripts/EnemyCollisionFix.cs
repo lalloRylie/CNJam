167c9be1cada86929d244af0739fb018831424c8
Rylie Lallo <Rylie Lallo>
2016-02-13 23:41:50 -0800

    Sparko now pushes bitches out of the way if he accidentally ends up within their collider

--- Assets/Scripts/EnemyScripts/EnemyCollisionFix.cs
+++ Assets/Scripts/EnemyScripts/EnemyCollisionFix.cs
@@ -5,7 +5,24 @@ public class EnemyCollisionFix : MonoBehaviour {
 
     void OnCollisionStay2D(Collision2D coll)
     {
-
+        float offset = 0.2f;
+        //If this enemy is to the left of Sparko
+        if (transform.parent.position.x < coll.transform.position.x)
+        {
+            //coll.transform.position = transform.parent.position - (GetComponent<Collider2D>().bounds.extents + coll.gameObject.GetComponentInChildren<Collider2D>().bounds.extents + new Vector3(offset, 0f));
+            Vector3 newPos = coll.transform.position - (GetComponent<Collider2D>().bounds.extents + coll.gameObject.GetComponentInChildren<Collider2D>().bounds.extents + new Vector3(offset, 0f));
+            newPos.y = transform.parent.position.y;
+            newPos.z = 0f;
+            transform.parent.position = newPos;
+        }
+        else
+        {
+            //coll.transform.parent.position = transform.parent.position + (GetComponent<Collider2D>().bounds.extents + coll.gameObject.GetComponentInChildren<Collider2D>().bounds.extents + new Vector3(offset, 0f));
+            Vector3 newPos = coll.transform.position + (GetComponent<Collider2D>().bounds.extents + coll.gameObject.GetComponentInChildren<Collider2D>().bounds.extents + new Vector3(offset, 0f));
+            newPos.y = transform.parent.position.y;
+            newPos.z = 0f;
+            transform.parent.position = newPos;
+        }
     }
         // Use this for initialization
         void Start () {
