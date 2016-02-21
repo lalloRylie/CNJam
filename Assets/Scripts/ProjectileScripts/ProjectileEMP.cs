999cba862422afada8c91131df60b63a2e437986
Rylie Lallo <Rylie Lallo>
2016-02-20 18:19:50 -0800

    emp projectile code added to animation script

---
+++ Assets/Scripts/ProjectileScripts/ProjectileEMP.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ProjectileEMP : MonoBehaviour {
+
+    float counter = 0f;
+
+    // Use this for initialization
+    void Start()
+    {
+        //modifies alpha of the EMP blast sprite
+        GetComponentInChildren<SpriteRenderer>().color = new Color(1f, 1f, 1f, 0.8f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        counter += 20f * Time.deltaTime;
+        Vector3 newScaleVector = new Vector3(transform.localScale.x, transform.localScale.y / counter, 1f);
+        transform.localScale = newScaleVector;
+
+        if (counter > 10f)
+            Destroy(gameObject);
+    }
+}
