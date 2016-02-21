02ed42ec8a834cef136643226b8706b1201240b1
Rylie Lallo <Rylie Lallo>
2016-02-20 18:55:42 -0800

    EMP/Board wipe animation complete. NEEDS A FRAME OR TWO

--- Assets/Scripts/ProjectileScripts/ProjectileEMP.cs
+++ Assets/Scripts/ProjectileScripts/ProjectileEMP.cs
@@ -15,8 +15,13 @@ public class ProjectileEMP : MonoBehaviour {
     // Update is called once per frame
     void Update()
     {
-        counter += 20f * Time.deltaTime;
-        Vector3 newScaleVector = new Vector3(transform.localScale.x, transform.localScale.y / counter, 1f);
+        counter += 10 * Time.deltaTime;
+        Vector3 newScaleVector = new Vector3(transform.localScale.x + (1.5f * counter), transform.localScale.y + counter, 1f);
+
+
+        if (counter > 2.5f)
+            newScaleVector = new Vector3(transform.localScale.x / (counter * 1.5f), transform.localScale.y / (counter * 1.5f), 1f);
+
         transform.localScale = newScaleVector;
 
         if (counter > 10f)
