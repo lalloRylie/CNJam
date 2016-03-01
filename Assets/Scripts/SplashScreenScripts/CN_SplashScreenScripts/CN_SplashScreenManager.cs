d82d10afcbf17b3d279380e9e1bc35e3ac25420b
Devin Stewart <Devin Stewart>
2016-03-01 04:13:08 -0800

    tweaked game mode settings

---
+++ Assets/Scripts/SplashScreenScripts/CN_SplashScreenScripts/CN_SplashScreenManager.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CN_SplashScreenManager : MonoBehaviour {
+
+    float timer = 0f;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        timer += 1f * Time.deltaTime;
+
+        if(timer > 1.5f) {
+            timer = 0f;
+            Application.LoadLevel("MI_SplashScreen");
+        }
+	}
+}
